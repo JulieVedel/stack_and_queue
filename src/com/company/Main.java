00e8db991823114a7de3316bf6377cf4d140cbe4
JulieVedel <juli76k0@edu.easj.dk>
2022-03-21 11:09:54 +0100

    Construction of queue class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-		Stack s  = new Stack(5);
+		/*Stack s  = new Stack(5);
 		s.push(4);
 		s.push(1);
 		s.push(94);
@@ -24,6 +24,27 @@ public class Main {
 		s.push(6);
 		s.push(10);
 
-		System.out.println(s.getSize());
+		System.out.println(s.getSize());*/
+
+		Queue q = new Queue(7);
+		q.offer(8);
+		q.offer(5);
+		q.offer(13);
+
+		q.remove();
+
+		System.out.println(q.peek());
+		System.out.println(q.empty());
+
+		q.remove();
+		q.remove();
+
+		System.out.println(q.empty());
+		System.out.println(q.getSize());
+
+		q.offer(9);
+		q.offer(25);
+
+		System.out.println(q.getSize());
     }
 }
--- src/com/company/Queue.java
+++ src/com/company/Queue.java
@@ -1,4 +1,57 @@
 package com.company;
 
 public class Queue {
+    private int[] elements;
+    private int size;
+    public static final int DEFAULT_CAPACITY = 16;
+
+    /** Construct a queue with the default capacity 16 */
+    public Queue() {
+        this(DEFAULT_CAPACITY);
+        elements = new int[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    /** Construct a queue with the specified maximum capacity */
+    public Queue(int capacity) {
+        elements = new int[capacity];
+        size = 0;
+    }
+
+    /** Insert a new integer into the queue */
+    public void offer(int value) {
+        elements[size] = value;
+        size++;
+    }
+
+
+    /** Return and remove the element from the queue */
+    public int remove() {
+        int element = elements[0];
+        for (int i = 1; i < elements.length; i++) {
+            elements[i - 1] = elements[i];
+        }
+        size--;
+        System.out.println("Remove the next element in the queue: " + element);
+        return element;
+    }
+
+    /** Return the element from the queue, which has been in the queue longest time */
+    public int peek() {
+        return elements[0];
+    }
+
+    /** Test whether the queue is empty */
+    public boolean empty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Return the number of elements in the queue */
+    public int getSize() {
+        return size;
+    }
+
 }
